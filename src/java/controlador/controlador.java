c28697d231bc3100533d2bef726b53cfa7e2d010
Gloria <Gloria@192.168.0.11>
2019-08-15 14:53:49 -0300

    creamos una extension java, para que sirva de controlador para el index.jsp. creamos dos atributos por defecto.} creamos 2 variables para que reciban los datos del login de index.jsp

---
+++ src/java/controlador/controlador.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Desarrollo Web
+ */
+public class controlador extends HttpServlet {
+    //creamos dos atributos por defecto.
+    String usurioPorDefecto="nico";
+    String contraseñaPordefecto="37685367";
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        //para recibir datos desde el index.jsp.
+        String nombre;
+        String contraseña;
+        //recibe el dato de index.jsp y se especifica el nombre de lo que queres que reciba. casteamos con String al principio.
+        nombre=(String)request.getAttribute("usuario");
+        contraseña=(String)request.getAttribute("contraseña");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
